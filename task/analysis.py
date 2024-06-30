ff70802e260b33452fb743f2c3f6896bdab89b8b
ArthPrajapati2 <aprajapati2@conestogac.on.ca>
2024-06-30 18:08:29 -0400

    improve the data

--- task/analysis.py
+++ task/analysis.py
@@ -19,6 +19,23 @@ general_df = pd.concat([general_df, prenatal_df, sports_df], ignore_index=True)
 
 # Drop the 'Unnamed: 0' column from the concatenated DataFrame
 general_df.drop(columns=['Unnamed: 0'], inplace=True)
+# Drop rows where all elements are missing
+general_df.dropna(axis='rows', inplace=True, how='all')
+
+# Replace 'male' and 'man' with 'm' in the gender column
+general_df.replace(['male', 'man'], 'm', inplace=True)
+
+# Replace 'female' and 'woman' with 'f' in the gender column
+general_df.replace(['female', 'woman'], 'f', inplace=True)
+
+# Fill NaN values in the 'gender' column with 'f'
+general_df.fillna({'gender': 'f'}, inplace=True)
+
+# Fill NaN values in specified columns with 0
+general_df.fillna({'bmi': 0, 'diagnosis': 0, 'blood_test': 0, 'ecg': 0, 'ultrasound': 0, 'mri': 0, 'xray': 0, 'children': 0, 'months': 0}, inplace=True)
+
+# Print the shape of the resulting DataFrame
+print(general_df.shape)
 
 # Sample 20 rows from the DataFrame with a fixed random state for reproducibility
 general_df = general_df.sample(n=20, random_state=30)
